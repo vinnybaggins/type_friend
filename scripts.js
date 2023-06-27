2b3fd6a739134279a6dbf3d76229e126b1b5b6d7
Vinícius Oliveira <114959073+vinnybaggins@users.noreply.github.com>
2023-06-27 16:09:17 -0300

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,77 +1,52 @@
-const buttonContainer = document.querySelector('#buttonContainer');
+const magicBtn = document.querySelector("#magicBtn");
+const inputContainer = document.querySelector("#inputContainer");
+const friendInput = document.querySelector("#friendInput");
 
-const magicBtn = document.querySelector('#magicBtn');
-
-
-const inputContainer = document.querySelector('#inputContainer');
-
-const friendInput = document.querySelector('#friendInput');
-
-const clearPassBtn = document.querySelector('#clearPassBtn');
-
+const clearPassBtn = document.querySelector("#clearPassBtn");
 const showPassBtn = document.querySelector("#showPassBtn");
-
+const hidePassBtn = document.querySelector("#hidePassBtn");
 const reappearBtn = document.querySelector("#reappearBtn");
 
 const balrogContainer = document.querySelector("#balrogContainer");
 
-
-inputContainer.addEventListener(
-    "keyup", function(event){
-    
-    if(event.key === 'Enter'){
-        reappearBtn.click();    
+inputContainer.addEventListener("keyup", function (event) {
+    if (event.key === "Enter") {
+        reappearBtn.click();
     }
-})
+});
 
-function show(el){
+function show(el) {
     el.classList.remove("hide");
 }
 
-function hide(el){
+function hide(el) {
     el.classList.add("hide");
 }
 
-magicBtn.addEventListener(
-    'click', function(){
-        
-    hide(buttonContainer);
-    show(inputContainer);
-    
-})
-
-clearPassBtn.addEventListener(
-    'click', function(){
-    
+clearPassBtn.addEventListener("click", function () {
     friendInput.value = "";
-})
+});
 
-showPassBtn.addEventListener(
-    'click', function(){
+showPassBtn.addEventListener("click", function () {
+        friendInput.type = "text";
+        show (hidePassBtn); hide (showPassBtn);
+});
 
-        if(friendInput.type === "password"){
-            friendInput.type='text';
-            showPassBtn.innerText='Hide password';
-        }else{
-            friendInput.type='password';
-            showPassBtn.innerText='Show password';
-        }
+hidePassBtn.addEventListener("click", function () {
+        friendInput.type = "password";
+        hide (hidePassBtn); show (showPassBtn);
+});
 
-})
+reappearBtn.addEventListener("click", function () {
+    let input = friendInput.value;
+    input = input.toUpperCase();
 
-reappearBtn.addEventListener(
-    'click', function(){
-        
-        let input = friendInput.value;
-        input = input.toUpperCase();
-
-        if(input === "MELLON"){
-            inputContainer.classList.add("slideUp");
-            balrogContainer.classList.add("slideUp");
-            show(balrogContainer);
-        }else if(input === "FRIEND"){
-            alert("Speak in elvish: it's mellon")
-        }
-        friendInput.value = "";
-    
-})
+    if (input === "MELLON") {
+        inputContainer.classList.add("slideUp");
+        balrogContainer.classList.add("slideUp");
+        show(balrogContainer);
+    } else if (input === "AMIGO") {
+        alert("Fale em élfico: a resposta é Mellon");
+    }
+    friendInput.value = "";
+});
